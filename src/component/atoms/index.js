29fb535c7c880630e4a536dd12b61bd0fdf145e6
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-03 13:01:04 +0700

    slicing sign in Login Screen

--- src/component/atoms/index.js
+++ src/component/atoms/index.js
@@ -1,7 +1,11 @@
 import Button from './Button';
-import Gap from './Gap'
+import Gap from './Gap';
+import Input from './Input';
+import Link from './Link';
 
 export {
   Button,
-  Gap
+  Gap,
+  Input,
+  Link
 };
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { ILLogo } from '../../assets';
+import { Input, Link, Button, Gap } from '../../component';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.page}>
+      <ILLogo/>
+      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+      <Input label='Email Address'/>
+      <Gap height={24}/>
+      <Input label='Password'/>
+      <Gap height={10}/>
+      <Link title="Forgot My Password" size={12}/>
+      <Gap height={40}/>
+      <Button title='Sign In'/>
+      <Gap height={30}/>
+      <Link title="Create New Account" size={16} align='center'/>
     </View>
   )
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    padding: 40,
+    backgroundColor: 'white'
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Nunito-SemiBold', 
+    color: '#112340',
+    marginTop: 40,
+    marginBottom: 40,
+    maxWidth: 153
+  }
+});
