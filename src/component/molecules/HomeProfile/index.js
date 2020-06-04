51c930dd17670c0423f097cc74b53df9bb78ff5e
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-04 14:25:42 +0700

    create component and doctor screen

---
+++ src/component/molecules/HomeProfile/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {colors, fonts} from '../../../utils';
+import { DummyUser } from '../../../assets';
+
+const HomeProfile = () => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Image source={DummyUser} style={styles.avatar} />
+      <View>
+        <Text style={styles.name}>Shayna Melinda</Text>
+        <Text style={styles.profession}>Product Design</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default HomeProfile;
+
+const styles = StyleSheet.create({
+  container: {flexDirection: 'row'},
+  avatar: {width: 46, height: 46, borderRadius: 46 / 2, marginRight: 12},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    textTransform: 'capitalize',
+  },
+  profession: {
+    fontSize: 12,
+    fontFamily: fonts.primary[400],
+    color: colors.text.secondary,
+    textTransform: 'capitalize',
+  },
+});
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,14 +1,90 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { HomeProfile, DoctorCategory, RatedDoctor, NewsItem, Gap } from '../../component';
+import { colors, fonts } from '../../utils';
 
 const Doctor = () => {
   return (
-    <View>
-      <Text>Doctor Page</Text>
+    <View style={styles.page}>
+      <View style={styles.content}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.wrapperSection}>
+            <Gap height={30}/>
+            <HomeProfile/>
+            <Text style={styles.welcome}>
+              Mau konsultasi dengan siapa hari ini?
+            </Text>
+          </View>
+          <View style={styles.wrapperScroll}>
+            <ScrollView 
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              <View style={styles.category}>
+                <Gap width={32}/>
+                <DoctorCategory/>
+                <DoctorCategory/>
+                <DoctorCategory/>
+                <DoctorCategory/>
+                <Gap width={22}/>
+              </View>
+            </ScrollView>
+          </View>
+          <View style={styles.wrapperSection}>
+            <Text style={styles.sectionLabel}>Top Rated Doctor</Text>
+            <RatedDoctor/>
+            <RatedDoctor/>
+            <RatedDoctor/>
+            <Text style={styles.sectionLabel}>Good News</Text>
+          </View>
+          <NewsItem/>
+          <NewsItem/>
+          <NewsItem/>
+          <Gap height={30}/>
+        </ScrollView>
+      </View>
     </View>
   )
 }
 
 export default Doctor;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20
+  },
+  wrapperSection:{
+    paddingHorizontal: 16
+  },
+  welcome: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
+    maxWidth: 209
+  },
+  category: {
+    flexDirection: 'row'
+  },
+  wrapperSection: {
+    paddingHorizontal: 16
+  },
+  wrapperScroll: {
+    marginHorizontal: -16
+  },
+  sectionLabel: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16
+  }
+})
