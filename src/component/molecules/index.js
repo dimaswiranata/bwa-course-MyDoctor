5be2685a048883cdc58150f07d680486849494a5
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-07 20:19:36 +0700

    add flash message and loading component

--- src/component/molecules/index.js
+++ src/component/molecules/index.js
@@ -10,6 +10,7 @@ import ChatItem from './ChatItem';
 import InputChat from './InputChat';
 import Profile from './Profile';
 import ProfileItem from './ProfileItem';
+import Loading from './Loading';
 
 export {
   Header,
@@ -23,5 +24,6 @@ export {
   ChatItem,
   InputChat,
   Profile,
-  ProfileItem
+  ProfileItem,
+  Loading
 };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Header, Input, Button, Gap } from '../../component';
+import { Header, Input, Button, Gap, Loading } from '../../component';
 import { colors, useForm } from '../../utils';
+import { Fire } from '../../config';
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 const Register = ({navigation}) => {
-  // const [fullName, setFullName] = useState('');
-  // const [profession, setProfession] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-
   const [form, setForm] = useForm({
     fullName: '',
     profession: '',
@@ -16,51 +13,76 @@ const Register = ({navigation}) => {
     password: ''
   });
 
+  const [loading, setLoading] = useState(false)
+
   const onContinue = () => {
     console.log(form);
+    setLoading(true);
+    Fire.auth()
+      .createUserWithEmailAndPassword(form.email, form.password)
+      .then(success => {
+        setLoading(false);
+        setForm('reset');
+        console.log('register success: ', success)
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        setLoading(false);
+        showMessage({
+          message: errorMessage,
+          type: "default",
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+        console.log('error: ', error);
+      });
     // navigation.navigate('UploadPhoto')
   }
 
   return (
-    <View style={styles.page}>
-      <Header 
-        onPress={() => navigation.goBack()} 
-        title="Daftar Akun"
-      />
-      <View style={styles.content}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Input 
-            label='Full Name' 
-            value={form.fullName}
-            onChangeText={(value) => setForm('fullName', value)}
-          />
-          <Gap height={24}/>
-          <Input 
-            label='Pekerjaan' 
-            value={form.profession}
-            onChangeText={(value) => setForm('profession', value)}
-          />
-          <Gap height={24}/>
-          <Input 
-            label='Email' 
-            value={form.email}
-            onChangeText={(value) => setForm('email', value)}
-          />
-          <Gap height={24}/>
-          <Input 
-            label='Password' 
-            value={form.password}
-            onChangeText={(value) => setForm('password', value)}
-            secureTextEntry
-          />
-          <Gap height={40}/>
-          <Button 
-            title="Continue" 
-            onPress={onContinue}
-          />
-        </ScrollView>
+    <>
+      <View style={styles.page}>
+        <Header 
+          onPress={() => navigation.goBack()} 
+          title="Daftar Akun"
+        />
+        <View style={styles.content}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Input 
+              label='Full Name' 
+              value={form.fullName}
+              onChangeText={(value) => setForm('fullName', value)}
+            />
+            <Gap height={24}/>
+            <Input 
+              label='Pekerjaan' 
+              value={form.profession}
+              onChangeText={(value) => setForm('profession', value)}
+            />
+            <Gap height={24}/>
+            <Input 
+              label='Email' 
+              value={form.email}
+              onChangeText={(value) => setForm('email', value)}
+            />
+            <Gap height={24}/>
+            <Input 
+              label='Password' 
+              value={form.password}
+              onChangeText={(value) => setForm('password', value)}
+              secureTextEntry
+            />
+            <Gap height={40}/>
+            <Button 
+              title="Continue" 
+              onPress={onContinue}
+            />
+          </ScrollView>
+        </View>
       </View>
-    </View>
+      {/* loading */}
+      {loading && <Loading/>}
+    </>
   )
 }
 
