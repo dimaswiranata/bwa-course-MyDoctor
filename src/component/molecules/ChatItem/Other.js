5b384bc67318307bc8e52992126893ab729e6b69
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-12 12:27:30 +0700

    finished messages & chatting page

--- src/component/molecules/ChatItem/Other.js
+++ src/component/molecules/ChatItem/Other.js
@@ -3,15 +3,15 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { fonts, colors } from '../../../utils';
 import { DummyDoctor9 } from '../../../assets';
 
-const Other = () => {
+const Other = ({text, date, photo}) => {
   return (
     <View style={styles.container}>
-      <Image source={DummyDoctor9} style={styles.avatar}/>
+      <Image source={photo} style={styles.avatar}/>
       <View>
         <View style={styles.chatContent}>
-          <Text style={styles.text}>Ibu dokter, apakah memakan jeruk tiap hari itu buruk?</Text>
+          <Text style={styles.text}>{text}</Text>
         </View>
-        <Text style={styles.date}>4.20 AM</Text>
+        <Text style={styles.date}>{date}</Text>
       </View>
     </View>
   )
